4fa474be20e5972b5d3c0f9695e935bcdfa4b43a
patdy074 <149289275+patdy074@users.noreply.github.com>
2025-06-14 16:04:51 +0930

    Extend PatientFileLoader with Adapter interface
    - Added AbstractPatientDatabaseLoader as base class
    - Declared loadPatients() override in .h file
    - Allows replacing data source with file input via one-line change

--- dpcpp-2025-ass2-clean-v4/PatientSystem/PatientFileLoader.h
+++ dpcpp-2025-ass2-clean-v4/PatientSystem/PatientFileLoader.h
@@ -2,14 +2,17 @@
 
 #include <string>
 #include <vector>
-
+#include "AbstractPatientDatabaseLoader.h"
 
 // forward declare the patient class
 class Patient;
 
-class PatientFileLoader
+class PatientFileLoader : public AbstractPatientDatabaseLoader
 {
 public:
+	//adapter that will load patients from a text file.
+	void loadPatients(std::vector<Patient*>& patients) override;
+
 	// loads a list of patients from a file and returns a vector of those patients
 	std::vector<Patient*> loadPatientFile(const std::string& file);
 
